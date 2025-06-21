0224c0d7c27585a5b804224b59d28cc08fb08fcd
Abhinav bargi <abhinavbargi.ab@gmail.com>
2025-06-21 22:02:02 +0530

    added username and password

--- skill_builder_app/lib/2_User_Interface/login_screen.dart
+++ skill_builder_app/lib/2_User_Interface/login_screen.dart
@@ -1,7 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required String hint,
+    required IconData icon,
+    TextInputType? keyboardType,
+    bool isPassword = false,
+    bool isPasswordVisible = false,
+    VoidCallback? onTogglePassword,
+  }) 
+  {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFF2D3748),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[50],
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: Colors.grey[200]!),
+          ),
+          child: TextField(
+            controller: controller,
+            keyboardType: keyboardType,
+            obscureText: isPassword && !isPasswordVisible,
+            decoration: InputDecoration(
+              hintText: hint,
+              prefixIcon: Icon(icon, color: Colors.grey[400]),
+              suffixIcon: isPassword
+                  ? IconButton(
+                      icon: Icon(
+                        isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.grey[400],
+                      ),
+                      onPressed: onTogglePassword,
+                    )
+                  : null,
+              border: InputBorder.none,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              hintStyle: TextStyle(color: Colors.grey[400]),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+
 class LoginScreen extends StatelessWidget {
+  
   LoginScreen({super.key});
   final TextEditingController email = TextEditingController();
   final TextEditingController pass = TextEditingController();
@@ -47,9 +104,11 @@ class LoginScreen extends StatelessWidget {
                       children: [
                         SizedBox(height: 20),
                         Text("Welcome to LOGIN", style: GoogleFonts.hanuman(fontSize: 25)),
+                        
                         SizedBox(height: 50),
-                        TextField(controller: email,decoration: InputDecoration(labelText: "Enter email"),)
-                      
+                        _buildTextField(controller: email, label: "Email", hint: "Enter Email ID", icon: Icons.email),
+                        SizedBox(height: 40),
+                        _buildTextField(controller: pass, label:"Password", hint: "Enter  the Password", icon: Icons.password,isPassword: true),
                       ],
                     ),
                   ),
