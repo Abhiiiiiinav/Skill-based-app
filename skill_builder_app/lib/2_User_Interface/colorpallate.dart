795c26f1dbddeb518341c555c954f931173c4a1b
Abhinav bargi <abhinavbargi.ab@gmail.com>
2025-06-22 18:20:48 +0530

    added a color pallete

--- skill_builder_app/lib/2_User_Interface/colorpallate.dart
+++ skill_builder_app/lib/2_User_Interface/colorpallate.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   // Primary Colors
   static const Color primaryNavy = Color(0xFF1A365D);       // rgb(26, 54, 93)
-  static const Color secondaryCream = Color(0xFFFAF9F7);    // rgb(250, 249, 247)
+  static const Color secondaryCream = Color.fromARGB(255, 255, 254, 251);    // rgb(250, 249, 247)
   static const Color accentElectric = Color(0xFF3182CE);    // rgb(49, 130, 206)
   static const Color accentCherry = Color(0xFFDC2626);      // rgb(220, 38, 38)
-  static const Color successEmerald = Color(0xFF10B981);    // rgb(16, 185, 129)
+  static const Color successEmerald = Color.fromARGB(255, 15, 184, 127);    // rgb(16, 185, 129)
   static const Color warningAmber = Color(0xFFF59E0B);      // rgb(245, 158, 11)
 
   // Neutral Colors
