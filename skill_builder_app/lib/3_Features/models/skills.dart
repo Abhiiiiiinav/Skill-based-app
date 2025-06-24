aed6ed56e22a2da80fbb77ef2fd201a917162852
Abhinav bargi <abhinavbargi.ab@gmail.com>
2025-06-24 21:25:14 +0530

    worked on Skill model

--- skill_builder_app/lib/3_Features/models/skills.dart
+++ skill_builder_app/lib/3_Features/models/skills.dart
@@ -1,24 +1,49 @@
+import 'task.dart';
+
 class Skills {
   final String id;
   final String title;
-  final String ?category;
+  final String? category;
   final int completedDays;
   final int totalDays;
-  final List<String> ?tasks;
+  final List<Task>? tasks;
 
   Skills({
-  required this.id,
-  required this.title,
-  this.category,
-  required this.totalDays,
-  required this.completedDays,
-  this.tasks
+    required this.id,
+    required this.title,
+    this.category,
+    required this.totalDays,
+    required this.completedDays,
+    this.tasks,
+  });
 
-});
+  
+  double get progress {
+    if (tasks == null || tasks!.isEmpty) return 0.0;
+    final completedTasks = tasks!.where((task) => task.isCompleted).length;
+    return completedTasks / tasks!.length;
+  }
 
+  
+  factory Skills.fromMap(Map<String, dynamic> map, String id) {
+    return Skills(
+      id: id,
+      title: map['title'] ?? '',
+      category: map['category'],
+      completedDays: map['completedDays'] ?? 0,
+      totalDays: map['totalDays'] ?? 0,
+      tasks: []
+    );
+  }
+
+  
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'category': category,
+      'completedDays': completedDays,
+      'totalDays': totalDays,
+      
+    };
+  }
 }
-
-
-
-
-
--- skill_builder_app/lib/main.dart
+++ skill_builder_app/lib/main.dart
@@ -9,7 +9,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName:".env");
-  await Firebase.initializeApp(options: FirebaseOptions(apiKey: dotenv.env['FIREBASE_API_KEY']!, appId: dotenv.env['FIREBASE_APP_ID']!, messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!, projectId: dotenv.env['FIREBASE_PROJECT_ID']!));
+  await Firebase.initializeApp(options: FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY']!, 
+    appId: dotenv.env['FIREBASE_APP_ID']!, 
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!, 
+    projectId: dotenv.env['FIREBASE_PROJECT_ID']!));
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
